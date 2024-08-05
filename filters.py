7bbc3278abbb9c7c8a21d85e5e92e5bdbc640be4
Quoc Huy <quochuy.working@gmail.com>
2024-08-05 23:46:38 +0700

    add class with function convolute filters

--- filters.py
+++ filters.py
@@ -25,3 +25,49 @@ def strokeEdges(src, dst, blurKsize = 7, edgeKsize = 5):
     
     # Merge the channels back into the destination image
     cv.merge(channels, dst)
+
+
+class VConvolutionFilter(object):
+    """A filter that applies a convolution to V (or all of BGR)."""
+    def __init__(self, kernel):
+        self._kernel = kernel
+    def apply(self, src, dst):
+        """Apply the filter with a BGR or gray source/destination."""
+        cv.filter2D(src, -1, self._kernel, dst)
+        
+        
+        
+class SharpenFilter(VConvolutionFilter):
+    """A sharpen filter with a 1-pixel radius."""
+    def __init__(self):
+        kernel = np.array([[-1, -1, -1],
+                              [-1,  9, -1],
+                              [-1, -1, -1]])
+        VConvolutionFilter.__init__(self, kernel)
+        
+class FindEdgesFilter(VConvolutionFilter):
+    """An edge-finding filter with a 1-pixel radius."""
+    def __init__(self):
+        kernel = np.array([[-1, -1, -1],
+                              [-1,  8, -1],
+                              [-1, -1, -1]])
+        VConvolutionFilter.__init__(self, kernel)
+
+class BlurFilter(VConvolutionFilter):
+    """A blur filter with a 2-pixel radius."""
+    def __init__(self):
+        kernel = np.array([[0.04, 0.04, 0.04, 0.04, 0.04],
+                              [0.04, 0.04, 0.04, 0.04, 0.04],
+                              [0.04, 0.04, 0.04, 0.04, 0.04],
+                              [0.04, 0.04, 0.04, 0.04, 0.04],
+                              [0.04, 0.04, 0.04, 0.04, 0.04]])
+        VConvolutionFilter.__init__(self, kernel)
+        
+        
+class EmbossFilter(VConvolutionFilter):
+    """An emboss filter with a 1-pixel radius."""
+    def __init__(self):
+        kernel = np.array([[-2, -1, 0],
+                              [-1,  1, 1],
+                              [ 0,  1, 2]])
+        VConvolutionFilter.__init__(self, kernel)
